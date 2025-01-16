60937b86894c07623843a4a654fc9b1d05fc084f
gisburn1999 <likeyourshirt@gmail.com>
2025-01-16 15:01:16 +0100

    added empty lines

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ def calculate(text_input):
     print("Welcome to our small calculator!")
     if text_input[1] == "+":
         return int(text_input[0])+int(text_input[-1])
+
+
 calculation = input("What do you want to calculate?")
 answer = calculate(calculation)
 print(f"The result is {answer}")
