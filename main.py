93ebd3aa5207c3b6b9a583a4759546083a509212
kayeffemm <kenneth.meier95@gmail.com>
2025-01-16 14:58:39 +0100

    Added welcome message

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def calculate(text_input):
+    print("Welcome to our small calculator!")
     if text_input[1] == "+":
         return int(text_input[0])+int(text_input[-1])
 calculation = input("What do you want to calculate?")
